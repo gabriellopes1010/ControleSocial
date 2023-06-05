b41aef8a195d56701d1b01b8e681b719ff37f349
Gabriel Lopes <gabriellopes1010@gmail.com>
2023-06-05 16:44:31 -0300

    Upload da pasta distribution com o arquivo javadoc

---
+++ ControleSocial/distribution/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Controller"},{"l":"Controller.TemplateMethod"},{"l":"Main"},{"l":"Model"},{"l":"View.Gui"},{"l":"View.Gui.Beneficiario"},{"l":"View.Gui.Empresa"},{"l":"View.Gui.Login"},{"l":"View.Gui.Menu"},{"l":"View.Gui.User"}];updateSearchResults();
